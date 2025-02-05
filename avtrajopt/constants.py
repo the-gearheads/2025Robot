b6b1c536686df60e484fe464b97dc7a0d5722aa6
person4268 <28717044+person4268@users.noreply.github.com>
2025-02-05 14:41:05 -0500

    armvator trajopt (no following code, constraints not accurate, waypoint impl should be improved)

---
+++ avtrajopt/constants.py
@@ -0,0 +1,24 @@
+import math
+
+N_samples_default = 400
+T_max = 10 # seconds
+dt_initial_guess = 0.05 # 50ms. bit big but works
+IN_TO_M = 0.0254
+elevator_min_len = 36 * IN_TO_M
+elevator_max_len = 80 * IN_TO_M
+
+pivot_min = math.radians(30)
+pivot_max = math.radians(90)
+
+endeff_x_min = 0
+endeff_x_max = 28.25 * IN_TO_M
+endeff_y_min = 0
+
+elevator_max_accel = 2 # m/s^2
+pivot_max_accel = 5 # rad/s^2
+
+def pivot_accel_scaling(elevator_length):
+  from jormungandr.autodiff import pow
+  return pivot_max_accel * pow(elevator_min_len / elevator_length, 2) # honestly no idea if this is correct but deepseek suggested it? really just a heuristic so it doesnt matter much until we do real testing
+
+  
