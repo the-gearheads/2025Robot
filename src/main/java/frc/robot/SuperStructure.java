321d5609c1a4a581715f51acd00ace410ef58daa
Ceticx0 <103392714+Ceticx0@users.noreply.github.com>
2025-02-18 21:03:59 -0500

    added endeff pose function

--- src/main/java/frc/robot/SuperStructure.java
+++ src/main/java/frc/robot/SuperStructure.java
@@ -1,9 +1,11 @@
 package frc.robot;
 
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.arm.Pivot;
 import frc.robot.subsystems.arm.Telescope;
+import frc.robot.subsystems.wrist.Wrist;
 import frc.robot.util.ArmvatorSample;
 import frc.robot.util.ArmvatorTrajectory;
 
@@ -14,6 +16,7 @@ public class SuperStructure {
 
   Pivot pivot;
   Telescope telescope;
+  Wrist wrist;
   public SuperStructure(Pivot pivot, Telescope telescope) {
     this.pivot = pivot;
     this.telescope = telescope;
@@ -28,6 +31,12 @@ public class SuperStructure {
 
   public Command followTrajectory(ArmvatorTrajectory traj) {
     return traj.follow(this::followSample, pivot, telescope);
+  }
+
+  public Translation2d getEndEffPos() {
+    double x = telescope.getPosition() * Math.cos(pivot.getAngle().getRadians());
+    double y = telescope.getPosition() * Math.sin(pivot.getAngle().getRadians());
+    return new Translation2d(x, y);
   }
 
 }
