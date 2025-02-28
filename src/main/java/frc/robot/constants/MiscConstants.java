18894019bf9c2933570a616d4905dd4c579f7236
Ceticx0 <103392714+Ceticx0@users.noreply.github.com>
2025-02-27 19:20:01 -0500

    added angle filtering

--- src/main/java/frc/robot/constants/MiscConstants.java
+++ src/main/java/frc/robot/constants/MiscConstants.java
@@ -1,10 +1,14 @@
 package frc.robot.constants;
+
+import edu.wpi.first.math.util.Units;
+
 /* One-off non subsystem-specific thinigs that aren't worth creating a full file for. If there's more than like 3 constants assosciated with one thing, just make a new file. */
 public final class MiscConstants {
   public static final double JOYSTICK_DEADBAND = 0.02;
   public static final int BREAK_COAST_BUTTON_PORT = 0;
 
   public static final double DISTANCE_TO_REEF = 0.45;
+  public static final double AUTO_ALIGN_FILTER_ANGLE = Units.degreesToRadians(70);
 
   // robot constants, unused; for reference
   public static final double ROBOT_MOI = 3.070789055; // kg * m^2   - for rinzlers drive base
--- src/main/java/frc/robot/util/ObjectiveTracker.java
+++ src/main/java/frc/robot/util/ObjectiveTracker.java
@@ -1,12 +1,14 @@
 package frc.robot.util;
 
+import static frc.robot.constants.MiscConstants.AUTO_ALIGN_FILTER_ANGLE;
+import static java.util.stream.Collectors.toList;
 
+import java.util.List;
 
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Transform2d;
 import frc.robot.subsystems.swerve.Swerve;
 
 public class ObjectiveTracker {
@@ -14,6 +16,7 @@ public class ObjectiveTracker {
   Swerve swerve;
   Pose2d predictedRobot;
   AprilTagFieldLayout layout;
+  Pose2d selectedPose;
 
   public ObjectiveTracker(Swerve swerve) {
     this.swerve = swerve;
@@ -22,49 +25,26 @@ public class ObjectiveTracker {
   public void periodic() {
   }
 
-
   public Pose2d getCoralObjective() {
-    Pose2d alignmentPose;
-    Transform2d errorPose;
-    Pose2d currentPose = new Pose2d();
-    double minAngle = 10;
-    double minDist = 2;  // m
     predictedRobot = swerve.getPose().exp(
         swerve.getRobotRelativeSpeeds().toTwist2d(lookAheadS));
+    // filter poses by angle
+    List<Pose2d> withinAnglePoses = ReefPositions.getReefPoses().stream()
+        .filter(pose -> Math.abs(
+            pose.getRotation().getRadians() - predictedRobot.getRotation().getRadians()) <= AUTO_ALIGN_FILTER_ANGLE)
+        .collect(toList());
+
+    if (withinAnglePoses.isEmpty()) {
+      selectedPose = null;
+    } else {
+      selectedPose = predictedRobot.nearest(withinAnglePoses);
+    }
+
     Logger.recordOutput("ObjectiveTracker/PredictedPose", predictedRobot);
-    Logger.recordOutput("ObjectiveTracker/Nearest", predictedRobot.nearest(ReefPositions.getReefPoses()));
-    // Pose2d[] test = new Pose2d[8];
-    // for (int i = 0; i < 8; i++) {
-    //   // -1 left, 1 right
-    //   alignmentPose = ReefPositions.getReefPose(i, -1);
-      // test[i] = alignmentPose;
-      // Logger.recordOutput("ObjectiveTracker/AlignmentPose", alignmentPose);
-      // errorPose = alignmentPose.minus(predictedRobot);
-      // alignmentPose.nearest(predictedRobot);
-      // Logger.recordOutput("ObjectiveTracker/ErrorPose", errorPose);
-      // // Logger.recordOutput("ObjectiveTracker/Error+Pred", errorPose.minus(predictedRobot));
-      // if (Math.abs(errorPose.getTranslation().getNorm()) < minDist) {
-      //   minDist = errorPose.getX() + errorPose.getY();
-      //   minAngle = errorPose.getRotation().getRadians();
-      //   currentPose = alignmentPose;
-      // }
-      // alignmentPose = ReefPositions.getReefPose(i, 1);
-      // Logger.recordOutput("ObjectiveTracker/AlignmentPose", alignmentPose);
-      // errorPose = alignmentPose.relativeTo(predictedRobot);
-      // Logger.recordOutput("ObjectiveTracker/ErrorPose", errorPose);
-      // Logger.recordOutput("ObjectiveTracker/Error+Pred", errorPose.plus(new Transform2d(predictedRobot.getTranslation(), predictedRobot.getRotation())));
-      // if (errorPose.getTranslation().getNorm() < minDist) {
-      //   if (errorPose.getRotation().getRadians() < minAngle) {
-      //     minDist = errorPose.getX() + errorPose.getY();
-      //     minAngle = errorPose.getRotation().getRadians();
-      //     currentPose = alignmentPose;
-      //   }
-      // }
-    // }
-    // Logger.recordOutput("ObjectiveTracker/test", test);
-    // Logger.recordOutput("ObjectiveTracker/SelectedPose", currentPose);
-    return currentPose;
+    Logger.recordOutput("ObjectiveTracker/NearestPose", predictedRobot.nearest(ReefPositions.getReefPoses()));
+    Logger.recordOutput("ObjectiveTracker/SelectedPose", selectedPose);
+    return selectedPose;
   }
 
-// 1/2 m to 20 deg
+  // 1/2 m to 20 deg
 }
