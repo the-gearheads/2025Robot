49e871123e48c3d198e9aef48565fdcf26253f6a
person4268 <28717044+person4268@users.noreply.github.com>
2025-03-13 10:58:41 -0400

    Revert "added two comments wow"
    This reverts commit 53428f4af31e96467ca9a8bbc4cda55c250518ad.

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -1,8 +1,12 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import frc.robot.subsystems.vision.CameraIntrinsics;
 
@@ -32,18 +36,13 @@ public class VisionConstants {
           new double[] { 0.03330603946791099,-0.028149602298339575,-3.2480645802923945E-4,-9.9467580835956E-5,-0.01790312270432083,-5.6026682560497186E-5,0.004799695107552726,0.002517423938318154 }),
       new CameraIntrinsics( // BACK LEFT
           1280, 800,
-          737.6136442454854, 733.1927575565593, 662.3371068271363, 435.9984845786,
-          new double[] { 0.15288116557227518, -0.2878953642242236, -0.0010986978034486703, 0.0011333394853758716,
-              0.12276685039910991 }),
-      new CameraIntrinsics(  // BACK_LEFT_2025
+          907.5481543996266, 907.2864231011899, 615.788934769545, 384.31897050631306,
+          new double[] { 0.04546920576986492,-0.060969029351137745,5.994746339424619E-5,-5.844773226102312E-4,0.008910675549702082,-0.0021265005451531174,0.00603543079528509,0.0019224441649826043 }),
+      new CameraIntrinsics( // BACK RIGHT
           1280, 800,
-          737.6136442454854, 733.1927575565593, 662.3371068271363, 435.9984845786,
-          new double[] { 0.15288116557227518, -0.2878953642242236, -0.0010986978034486703, 0.0011333394853758716,
-              0.12276685039910991 }),
-      new CameraIntrinsics(  // BACK_RIGHT
-          1280, 800,
-          737.6136442454854, 733.1927575565593, 662.3371068271363, 435.9984845786,
-          new double[] { 0.15288116557227518, -0.2878953642242236, -0.0010986978034486703, 0.0011333394853758716,
-              0.12276685039910991 })
+          903.6362625357887, 904.0894526094557, 659.272169009578, 419.33183667235414,
+          new double[] { 0.04817038713247634,-0.06653199679614717,2.440943204330648E-4,-1.1668007422068571E-4,0.010742825203920284,-0.00217144171086648,0.0051145983132926676,0.0012068741948585884 })
   };
+  public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(0.1, 0.1, Double.POSITIVE_INFINITY);
+  public static final Matrix<N3, N1> MULTI_TAG_STD_DEVS = VecBuilder.fill(0.1, 0.1, 0.1);
 }
