38c0ab747ef9bf61d1207180033391655cc2ee9f
London <londonariel919@gmail.com>
2025-03-17 18:38:35 -0400

    temp

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -13,7 +13,7 @@ public class VisionConstants {
   // sane values somewhere between 1e-7 and 10000. low == trust initial guess heading less.
   public static final double CONSTRAINED_PNP_HEADING_SCALE_FACTOR = 1;
   ;
-  public static final String[] CAMERA_NAMES = { "FRONT_LEFT_2025",  "BACK_LEFT_2025", "BACK_RIGHT"};
+  public static final String[] CAMERA_NAMES = { "FRONT_LEFT_2025 AA",  "BACK_LEFT_2025 AA", "BACK_RIGHT"};
   public static final Transform3d[] CAMERA_TRANSFORMS = {
       new Transform3d(  // FRONT_LEFT_2025
           new Translation3d(0.36322, 0.1778, 0.16891),
