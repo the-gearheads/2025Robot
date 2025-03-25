3d7eb7080245bbc6ad57d82715ee2084f8502a72
person4268 <28717044+person4268@users.noreply.github.com>
2025-03-25 15:31:32 -0400

    fixed the telescope min height being an inch too high

--- avtrajopt/constants.py
+++ avtrajopt/constants.py
@@ -4,7 +4,7 @@ N_samples_default = 200
 T_max = 10 # seconds
 dt_initial_guess = 0.005 # 50ms. bit big but works
 IN_TO_M = 0.0254
-elevator_min_len = 36 * IN_TO_M + 1 * IN_TO_M
+elevator_min_len = 36 * IN_TO_M ## + 1 * IN_TO_M
 elevator_max_len = 80 * IN_TO_M
 
 pivot_min = math.radians(13.5)
