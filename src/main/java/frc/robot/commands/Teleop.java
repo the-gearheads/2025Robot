42113a5b0fcbec25feea25384899db6455f29062
person4268 <28717044+person4268@users.noreply.github.com>
2025-03-31 14:37:12 -0400

    okay imma say this approach is also flawed, need to do sanity checks i dont wanna do rn

--- src/main/java/frc/robot/commands/Teleop.java
+++ src/main/java/frc/robot/commands/Teleop.java
@@ -1,19 +1,17 @@
 package frc.robot.commands;
 
-import static frc.robot.constants.MiscConstants.AUTO_ALIGN_ANGLE_THRESHOLD;
-import static frc.robot.constants.MiscConstants.AUTO_ALIGN_DIST_THRESHOLD;
 import static frc.robot.constants.SwerveConstants.DRIVE_FEEDFORWARD;
 import static frc.robot.constants.SwerveConstants.MAX_ROBOT_TRANS_SPEED;
+import static frc.robot.constants.SwerveConstants.WHEEL_POSITIONS;
 
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.controller.ImplicitModelFollower;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.system.LinearSystem;
@@ -22,13 +20,10 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.controllers.Controllers;
-import frc.robot.subsystems.intake.GamePiece;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.swerve.Swerve;
 import frc.robot.subsystems.vision.Vision;
-import frc.robot.util.AlignToPose;
 import frc.robot.util.ObjectiveTracker;
-import frc.robot.util.ReefPositions;
 
 public class Teleop extends Command {
     Swerve swerve;
@@ -77,37 +72,38 @@ public class Teleop extends Command {
     ChassisSpeeds finalSpeeds;
 
     // decide whether to do autoalign
-    Pose2d currentCoralTarget = AlignToPose.getCoralObjective(swerve.getPose(), x, y);
+    // Pose2d currentCoralTarget = AlignToPose.getCoralObjective(swerve.getPose(), x, y);
     
-    if (currentCoralTarget.getTranslation()
-            .getDistance(swerve.getPose().getTranslation()) < AUTO_ALIGN_DIST_THRESHOLD
-            && Math.abs(currentCoralTarget.getRotation().minus(swerve.getPose().getRotation()).getRadians()) < AUTO_ALIGN_ANGLE_THRESHOLD
-            && intake.getGamePiece() == GamePiece.CORAL
-            && !tracker.facingReef()) {
-        int nearestTagId = ReefPositions.getClosestReefTagId(currentCoralTarget);
-        Rotation2d gyroOffset = swerve.getPose().getRotation().minus(swerve.getPoseWheelsOnly().getRotation());
-        Logger.recordOutput("AlignToPose/TeleopAligning", true);
-        // turn on 2d vision
-        AlignToPose.enableReefVision(vision, gyroOffset, nearestTagId);
+    // if (currentCoralTarget.getTranslation()
+    //         .getDistance(swerve.getPose().getTranslation()) < AUTO_ALIGN_DIST_THRESHOLD
+    //         && Math.abs(currentCoralTarget.getRotation().minus(swerve.getPose().getRotation()).getRadians()) < AUTO_ALIGN_ANGLE_THRESHOLD
+    //         && intake.getGamePiece() == GamePiece.CORAL
+    //         && !tracker.facingReef()) {
+    //     int nearestTagId = ReefPositions.getClosestReefTagId(currentCoralTarget);
+    //     Rotation2d gyroOffset = swerve.getPose().getRotation().minus(swerve.getPoseWheelsOnly().getRotation());
+    //     Logger.recordOutput("AlignToPose/TeleopAligning", true);
+    //     // turn on 2d vision
+    //     AlignToPose.enableReefVision(vision, gyroOffset, nearestTagId);
 
-        // get final speeds
-        Pair<ChassisSpeeds, Double> autoAlignSpeeds = AlignToPose.getAutoAlignSpeeds(x, y, swerve.getPose());
-        ChassisSpeeds driverSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(x * (1 - autoAlignSpeeds.getSecond()), y * (1 - autoAlignSpeeds.getSecond()), rot * (1 - autoAlignSpeeds.getSecond())), fieldAdjustedRobotRot);
-        finalSpeeds = driverSpeeds.plus(autoAlignSpeeds.getFirst());
-    } else {
-        Logger.recordOutput("AlignToPose/TeleopAligning", false);
-        // return to normal vision
-        AlignToPose.disableReefVision(vision);
-        ChassisSpeeds driverSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(xSpeed, ySpeed, rotSpeed), fieldAdjustedRobotRot);
-        finalSpeeds = driverSpeeds;
-    }
+    //     // get final speeds
+    //     Pair<ChassisSpeeds, Double> autoAlignSpeeds = AlignToPose.getAutoAlignSpeeds(x, y, swerve.getPose());
+    //     ChassisSpeeds driverSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(x * (1 - autoAlignSpeeds.getSecond()), y * (1 - autoAlignSpeeds.getSecond()), rot * (1 - autoAlignSpeeds.getSecond())), fieldAdjustedRobotRot);
+    //     finalSpeeds = driverSpeeds.plus(autoAlignSpeeds.getFirst());
+    // } else {
+    //     Logger.recordOutput("AlignToPose/TeleopAligning", false);
+    //     // return to normal vision
+    //     AlignToPose.disableReefVision(vision);
+    //     ChassisSpeeds driverSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(xSpeed, ySpeed, rotSpeed), fieldAdjustedRobotRot);
+    //     finalSpeeds = driverSpeeds;
+    // }
+    finalSpeeds = new ChassisSpeeds(xSpeed, ySpeed, rotSpeed);
     finalSpeeds = implicitModelFollowing(finalSpeeds); // man this is gonna mess with autoalign but here goesssss
     // swerve.drive(new ChassisSpeeds(1,1, 0));
     swerve.drive(finalSpeeds);
   }
 
   SimpleMotorFeedforward desiredFF = new SimpleMotorFeedforward(0, DRIVE_FEEDFORWARD.getKv(), DRIVE_FEEDFORWARD.getKa() * 0.5);
-
+  SwerveDriveKinematics kinematics = new SwerveDriveKinematics(WHEEL_POSITIONS);
   LinearSystem<N1, N1, N1> currentSystem = LinearSystemId.identifyVelocitySystem(DRIVE_FEEDFORWARD.getKv(), DRIVE_FEEDFORWARD.getKa());
   LinearSystem<N1, N1, N1> desiredSystem = LinearSystemId.identifyVelocitySystem(desiredFF.getKv(), desiredFF.getKa());
 
@@ -129,11 +125,14 @@ public class Teleop extends Command {
 
     var desiredRobotSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(desiredSpeeds, rot);
 
-    SwerveModuleState[] states = swerve.kinematics.toSwerveModuleStates(desiredRobotSpeeds);
+    SwerveModuleState[] states = kinematics.toSwerveModuleStates(desiredRobotSpeeds);
+    Logger.recordOutput("Teleop/DesiredStates", states);
+
     for (int i = 0; i < 4; i++) {
       states[i].speedMetersPerSecond = followers[i].calculate(VecBuilder.fill(modStates[i].speedMetersPerSecond), VecBuilder.fill(desiredFF.calculate(states[i].speedMetersPerSecond))).get(0, 0);
     }
 
-    return swerve.kinematics.toChassisSpeeds(states);
+    Logger.recordOutput("Teleop/AdjustedStates", states);
+    return kinematics.toChassisSpeeds(states);
   }
 }
