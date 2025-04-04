5384e645e30a18f56aa22d7a4256c5909626fd70
person4268 <28717044+person4268@users.noreply.github.com>
2025-04-04 18:43:28 -0400

    increase hold times?

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -71,9 +71,9 @@ public class Intake extends SubsystemBase {
   public Command runIntake() {
     return run(() -> setVoltage(INTAKE_VOLTAGE)).until(this::hasGamePiece).andThen(Commands.waitSeconds(0.1)).andThen(Commands.defer(() -> {
       if(getGamePiece() == GamePiece.ALGAE) {
-        return Commands.waitSeconds(0.05);
+        return Commands.waitSeconds(0.2);
       } else {
-        return Commands.waitSeconds(0.5);
+        return Commands.waitSeconds(0.6);
       }
     }, Set.of(this)));
   }
